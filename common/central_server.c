09c90d2d6d5d3a8bff18037599c57f9c75bc70d9
root <praveenv542@gmail.com>
2015-09-12 11:35:24 +0530

    Dividing input file using split command

---
+++ common/central_server.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void db_server_divide(char *path, unsigned int job_id, unsigned int n)
+{
+   char buf[20];
+   char split[100] = "split -dl ";
+   char *temp = buf;
+   unsigned int count = 0;
+   FILE *fp = fopen(path, "r+");
+   while(fgets(buf, 20, fp))
+   {
+      count++;  
+   }
+   count = count / n;
+   printf("%u",count);
+   sprintf(buf,"%d", count);
+   strcat(split, buf);
+   strcat(split, " ");
+   strcat(split, path);
+   strcat(split, " ");
+   sprintf(buf,"%d", job_id);
+   strcat(split, buf);
+   strcat(split, "_"); 
+   printf("FINAL = %s",split);
+   system(split);
+   fclose(fp);
+}
+
+void main()
+{
+   db_server_divide("/home/praveen/syspgm/test_split", 1, 11);
+}
