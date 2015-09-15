c0ee795a34533b428cd3f94a906ba1b1f977eef7
ravisutrave <ravic.jc07@gmail.com>
2015-09-15 14:53:14 +0530

    Framework for calling APIs to solve specific task

---
+++ common/src/api.h
@@ -0,0 +1,27 @@
+#ifndef __API_H__
+#define __API_H__
+
+typedef enum api_id {
+    FIND_MAX = 1,
+
+    MAX_API /*Keep this always last*/
+} api_id_t;
+
+typedef enum API_STATUS_ {
+    API_FAILURE = 0,
+    API_SUCCESS,
+    API_INVALID_INPUT,
+    API_INVALID_ID,
+} api_status_t;
+
+api_status_t main_api(void *input, void *output, api_id_t id); 
+typedef api_status_t (*api_func)(void *, void *);
+typedef struct api_ {
+    api_func api;
+}api;
+
+typedef struct int_vector {
+    int *vector;
+    long long len;
+} int_vector_t;
+#endif /* __API_H__ */
