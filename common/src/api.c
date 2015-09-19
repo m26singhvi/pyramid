b4ebccfd24ed4b9aea432ecc120fa267d8d3ee3f
ravisutrave <ravic.jc07@gmail.com>
2015-09-19 15:54:20 +0530

    Fix compilation

--- common/src/api.c
+++ common/src/api.c
@@ -19,7 +19,7 @@ int lines_input_file(char *input){
         while(fgets ( line, sizeof line, file ) != NULL){
             num++;
         }
-    fclose(f);
+    fclose(file);
     }
     return num;
 }
@@ -41,7 +41,7 @@ api_status_t read_input_file(char *input , int_vector_t * output){
         }
         output->vector[count++] = num;
     }
-    fclose(f);
+    fclose(file);
     return API_SUCCESS;
 }
 
